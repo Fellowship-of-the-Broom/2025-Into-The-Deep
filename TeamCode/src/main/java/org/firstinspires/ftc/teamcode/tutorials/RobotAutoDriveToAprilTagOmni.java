53753c25fc4f08ffbd77044b93593d69dfb1871e
team25674 <team25674@broomfieldstem.org>
2025-10-04 15:54:39 -0600

    Made it so right bumper goes to red and left bumper goes to blue

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tutorials/RobotAutoDriveToAprilTagOmni.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tutorials/RobotAutoDriveToAprilTagOmni.java
@@ -27,13 +27,13 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package org.firstinspires.ftc.robotcontroller.external.samples;
+package org.firstinspires.ftc.teamcode.tutorials;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.Range;
+
 import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.controls.ExposureControl;
@@ -86,11 +86,10 @@ import java.util.concurrent.TimeUnit;
  */
 
 @TeleOp(name="Omni Drive To AprilTag", group = "Concept")
-@Disabled
 public class RobotAutoDriveToAprilTagOmni extends LinearOpMode
 {
     // Adjust these numbers to suit your robot.
-    final double DESIRED_DISTANCE = 12.0; //  this is how close the camera should get to the target (inches)
+    final double DESIRED_DISTANCE = 24.0; //  this is how close the camera should get to the target (inches)
 
     //  Set the GAIN constants to control the relationship between the measured position error, and how much power is
     //  applied to the drive motors to correct the error.
@@ -109,10 +108,12 @@ public class RobotAutoDriveToAprilTagOmni extends LinearOpMode
     private DcMotor backRightDrive = null;  //  Used to control the right back drive wheel
 
     private static final boolean USE_WEBCAM = true;  // Set true to use a webcam, or false for a phone camera
-    private static final int DESIRED_TAG_ID = -1;     // Choose the tag you want to approach or set to -1 for ANY tag.
+    private static final int TAG_BLUE = 20;
+    private static final int TAG_RED = 24;
     private VisionPortal visionPortal;               // Used to manage the video source.
     private AprilTagProcessor aprilTag;              // Used for managing the AprilTag detection process.
-    private AprilTagDetection desiredTag = null;     // Used to hold the data for a detected AprilTag
+    private AprilTagDetection desiredTagRed = null; // Used to hold the data for a detected AprilTag
+    private AprilTagDetection desiredTagBlue = null;// Used to hold the data for a detected AprilTag
 
     @Override public void runOpMode()
     {
@@ -152,7 +153,8 @@ public class RobotAutoDriveToAprilTagOmni extends LinearOpMode
         while (opModeIsActive())
         {
             targetFound = false;
-            desiredTag  = null;
+            desiredTagRed = null;
+            desiredTagBlue = null;
 
             // Step through the list of detected tags and look for a matching tag
             List<AprilTagDetection> currentDetections = aprilTag.getDetections();
@@ -160,40 +162,52 @@ public class RobotAutoDriveToAprilTagOmni extends LinearOpMode
                 // Look to see if we have size info on this tag.
                 if (detection.metadata != null) {
                     //  Check to see if we want to track towards this tag.
-                    if ((DESIRED_TAG_ID < 0) || (detection.id == DESIRED_TAG_ID)) {
+                    if (detection.id == TAG_RED) {
                         // Yes, we want to use this tag.
-                        targetFound = true;
-                        desiredTag = detection;
-                        break;  // don't look any further.
+                        desiredTagRed = detection;
+                    } else if (detection.id == TAG_BLUE) {
+                        // Yes, we want to use this tag.
+                        desiredTagBlue = detection;
                     } else {
                         // This tag is in the library, but we do not want to track it right now.
                         telemetry.addData("Skipping", "Tag ID %d is not desired", detection.id);
                     }
+
                 } else {
                     // This tag is NOT in the library, so we don't have enough information to track to it.
                     telemetry.addData("Unknown", "Tag ID %d is not in TagLibrary", detection.id);
                 }
             }
 
+            AprilTagDetection targetTag = null;
+            if(gamepad1.left_bumper && desiredTagBlue != null) {
+                targetTag = desiredTagBlue;
+                targetFound = true;
+            } else if (gamepad1.right_bumper && desiredTagRed != null) {
+                targetTag = desiredTagRed;
+                targetFound = true;
+            }
+
+
+
             // Tell the driver what we see, and what to do.
             if (targetFound) {
                 telemetry.addData("\n>","HOLD Left-Bumper to Drive to Target\n");
-                telemetry.addData("Found", "ID %d (%s)", desiredTag.id, desiredTag.metadata.name);
-                telemetry.addData("Range",  "%5.1f inches", desiredTag.ftcPose.range);
-                telemetry.addData("Bearing","%3.0f degrees", desiredTag.ftcPose.bearing);
-                telemetry.addData("Yaw","%3.0f degrees", desiredTag.ftcPose.yaw);
+                telemetry.addData("Found", "ID %d (%s)", targetTag.id, targetTag.metadata.name);
+                telemetry.addData("Range",  "%5.1f inches", targetTag.ftcPose.range);
+                telemetry.addData("Bearing","%3.0f degrees", targetTag.ftcPose.bearing);
+                telemetry.addData("Yaw","%3.0f degrees", targetTag.ftcPose.yaw);
             } else {
                 telemetry.addData("\n>","Drive using joysticks to find valid target\n");
             }
 
             // If Left Bumper is being pressed, AND we have found the desired target, Drive to target Automatically .
-            if (gamepad1.left_bumper && targetFound) {
+            if (targetFound) {
 
                 // Determine heading, range and Yaw (tag image rotation) error so we can use them to control the robot automatically.
-                double  rangeError      = (desiredTag.ftcPose.range - DESIRED_DISTANCE);
-                double  headingError    = desiredTag.ftcPose.bearing;
-                double  yawError        = desiredTag.ftcPose.yaw;
-
+                double  rangeError      = (targetTag.ftcPose.range - DESIRED_DISTANCE);
+                double  headingError    = targetTag.ftcPose.bearing;
+                double  yawError        = targetTag.ftcPose.yaw;
                 // Use the speed and turn "gains" to calculate how we want the robot to move.
                 drive  = Range.clip(rangeError * SPEED_GAIN, -MAX_AUTO_SPEED, MAX_AUTO_SPEED);
                 turn   = Range.clip(headingError * TURN_GAIN, -MAX_AUTO_TURN, MAX_AUTO_TURN) ;
